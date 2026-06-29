ad95af381a937fa6d5ef53bc819ddd1bddd2c4a5
Down to the Wire <8404598+wireddown@users.noreply.github.com>
2026-06-28 21:11:08 -0700

    tags.js: Also safely encode and decode '-' for js variable names

--- assets/js/tags.js
+++ assets/js/tags.js
@@ -1,12 +1,12 @@
 ---
 ---
 
-const tags = { {% for tag in site.tags %}{% capture tag_name %}{{ tag | first }}{% endcapture %}{{ tag_name | replace: " ", "_" }}: [{% for post in site.tags[tag_name] %}{ url: `{{ site.baseurl }}{{ post.url }}`, date: `{{post.date | date_to_string}}`, title: `{{post.title}}`},{% endfor %}],{% endfor %} }
+const tags = { {% for tag in site.tags %}{% capture tag_name %}{{ tag | first }}{% endcapture %}{{ tag_name | replace: " ", "_" | replace: "-", "__" }}: [{% for post in site.tags[tag_name] %}{ url: `{{ site.baseurl }}{{ post.url }}`, date: `{{post.date | date_to_string}}`, title: `{{post.title}}`},{% endfor %}],{% endfor %} }
 
 window.onload = function () {
   document.querySelectorAll(".tag").forEach((tag) => {
     tag.addEventListener("click", function (e) {
-      const lower_name = e.target.innerText.replace(" ","_");
+      const lower_name = e.target.innerText.replace(" ","_").replace("-", "__");
       const tag_keys = Object.keys(tags);
       const lower_keys = tag_keys.map((k) => k.toLocaleLowerCase());
       const key_index = lower_keys.indexOf(lower_name);
@@ -21,7 +21,7 @@ window.onload = function () {
         </a>
         `
       })
-      document.querySelector("#tag-modal-title").innerText = tag_name.replace("_"," ");
+      document.querySelector("#tag-modal-title").innerText = tag_name.replace("__","-").replace("_", " ");
       document.querySelector("#tag-modal-content").innerHTML = html;
       document.querySelector("#tag-modal-bg").classList.toggle("open");
       document.querySelector("#tag-modal").classList.toggle("open");
